961f486092c43d12c0454c7d98a41ff65fe529b9
Josh Graber <jgraber@renovo1.com>
2015-07-23 12:39:31 -0400

    Don't export the classes, just the interfaces.

--- typings/utility.d.ts
+++ typings/utility.d.ts
@@ -26,21 +26,6 @@ declare module rl.utilities.array {
             (item: TDataType): number;
         }): TDataType[];
     }
-    class ArrayUtility implements IArrayUtility {
-        findIndexOf<TDataType>(array: TDataType[], predicate: {
-            (item: TDataType): boolean;
-        }): number;
-        remove<TDataType>(array: TDataType[], item: TDataType | {
-            (obj: TDataType): boolean;
-        }): TDataType;
-        replace<TDataType>(array: TDataType[], oldItem: TDataType, newItem: TDataType): void;
-        sum<TDataType>(array: TDataType[], transform?: {
-            (item: TDataType): number;
-        }): number;
-        toDictionary<TDataType>(array: TDataType[], keySelector: {
-            (item: TDataType): string | number;
-        }): TDataType[];
-    }
 }
 declare module rl.utilities.observable {
     var moduleName: string;
@@ -61,19 +46,9 @@ declare module rl.utilities.observable {
         register: IRegisterFunction;
         fire(event?: string, ...params: any[]): void;
     }
-    class ObservableService implements IObservableService {
-        private watchers;
-        private nextKey;
-        register(action: {
-            (...params: any[]): void;
-        }, event?: string): IUnregisterFunction;
-        fire(event?: string, ...params: any[]): void;
-        private unregister(key);
-    }
     interface IObservableServiceFactory {
         getInstance(): IObservableService;
     }
-    function observableServiceFactory(): IObservableServiceFactory;
 }
 declare module rl.utilities.contentProvider {
     var moduleName: string;
@@ -86,23 +61,9 @@ declare module rl.utilities.contentProvider {
             (newText: JQuery): void;
         }, selector?: string): observable.IUnregisterFunction;
     }
-    class ContentProviderService implements IContentProviderService {
-        constructor(observableFactory: observable.IObservableServiceFactory);
-        private observable;
-        private content;
-        setContent(content: JQuery): void;
-        setTranscludeContent: {
-            (transcludeFunction: angular.ITranscludeFunction): void;
-        };
-        register(action: {
-            (newContent: JQuery): void;
-        }, selector?: string): observable.IUnregisterFunction;
-        getContent(selector?: string): JQuery;
-    }
     interface IContentProviderServiceFactory {
         getInstance(): IContentProviderService;
     }
-    function contentProviderServiceFactory(observableFactory: observable.IObservableServiceFactory): IContentProviderServiceFactory;
 }
 declare module rl.utilities.date {
     var moduleName: string;
@@ -115,13 +76,6 @@ declare module rl.utilities.date {
         getFullString(month: number): string;
         getDays(month: number, year?: number): number;
     }
-    class DateUtility {
-        constructor();
-        month: IMonth[];
-        private isLeapYear(year?);
-        getFullString(month: number): string;
-        getDays(month: number, year?: number): number;
-    }
 }
 declare module rl.utilities.jquery {
     var moduleName: string;
@@ -129,17 +83,11 @@ declare module rl.utilities.jquery {
     interface IJQueryUtility {
         replaceContent(contentArea: JQuery, newContents: JQuery): void;
     }
-    class JQueryUtility implements IJQueryUtility {
-        replaceContent(contentArea: JQuery, newContent: JQuery): void;
-    }
 }
 declare module rl.utilities.number {
     var moduleName: string;
     var serviceName: string;
     interface INumberUtility {
-        preciseRound(num: number, decimals: number): number;
-    }
-    class NumberUtility implements INumberUtility {
         preciseRound(num: number, decimals: number): number;
     }
 }
@@ -171,11 +119,6 @@ declare module rl.utilities.parentChildBehavior {
         getAllChildBehaviors<TBehavior>(childList: IChild<TBehavior>[]): TBehavior[];
         registerChildBehavior<TBehavior>(child: IChild<TBehavior>, behavior: TBehavior): void;
     }
-    class ParentChildBehaviorService {
-        getChildBehavior<TBehavior>(child: IChild<TBehavior>): TBehavior;
-        getAllChildBehaviors<TBehavior>(childList: IChild<TBehavior>[]): TBehavior[];
-        registerChildBehavior<TBehavior>(child: IChild<TBehavior>, behavior: TBehavior): void;
-    }
 }
 declare module rl.utilities.promise {
     var moduleName: string;
@@ -183,9 +126,6 @@ declare module rl.utilities.promise {
     interface IPromiseUtility {
         isPromise(promise: any): boolean;
         isPromise(promise: ng.IPromise<any>): boolean;
-    }
-    class PromiseUtility implements IPromiseUtility {
-        isPromise(promise: any): boolean;
     }
 }
 declare module rl.utilities.truncate {
@@ -196,7 +136,6 @@ declare module rl.utilities.truncate {
         (input?: string, truncateTo?: number, includeEllipses?: boolean): string;
         (input?: number, truncateTo?: number, includeEllipses?: boolean): string;
     }
-    function truncate(objectUtility: object.IObjectUtility): ITruncateFilter;
 }
 declare module rl.utilities {
     var moduleName: string;
